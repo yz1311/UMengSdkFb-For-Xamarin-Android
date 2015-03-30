94c95971a44442c71e276dfd39e466e487244403
yaozhenfa <976691141@qq.com>
2015-03-30 14:25:01 +0800

    Create Project

---
+++ Sample/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Com.Umeng.FB;
+
+namespace Sample
+{
+    [Activity(Label = "Sample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            FeedbackAgent agent = new FeedbackAgent(this);
+            agent.StartFeedbackActivity();
+        }
+    }
+}
+
